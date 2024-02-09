df708bc014fdc94c4732abddff1d8dd622569956
Adam <adam.malysz3@onet.com>
2024-02-09 20:45:57 +0100

    asda

---
+++ Assets/Wire.cs
@@ -0,0 +1,21 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wire : MonoBehaviour
+{
+    private UnityEngine.UI.Image image;
+    public void Initialize()
+    {
+       image = GetComponent<UnityEngine.UI.Image>();
+        SetColor(Color.black);
+    }
+
+    public void SetColor(Color color)
+    {
+        image.color = color;
+    }
+
+}
